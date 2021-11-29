69cf3ed5a9a2f1a09822b20fc4991ef538b77c78
eles linux <eleswastaken@gmail.com>
2021-11-29 17:56:58 +0600

    Linux setup

---
+++ main-data.js
@@ -0,0 +1,17 @@
+// module.exports =
+
+export default [
+    {
+        helo: "this is a string from the main-data.js",
+    },
+    {
+        imgURL: "",
+        imgAltText: "",
+        githubRepoURL: "",
+        livePreviewURL: "",
+        title: "",
+        description: "",
+        descriptionTitle: "",
+        date: new Date(),
+    }
+];
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import cards from "../../main-data.js";
+
+
+// implement insert cards into a container from cards
+
+console.log(cards)
