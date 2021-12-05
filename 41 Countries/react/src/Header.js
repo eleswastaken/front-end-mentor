e33b439d56584848e6e3522456ac1609d67e9a54
eles linux <eleswastaken@gmail.com>
2021-12-05 16:01:42 +0600

    Add theme switch button
    Bug to fix: on first load if the theme was light, the switch still will be on the dark side. Fix: get the switch element on reload and tranlate it. Problem: I don't know how to that in React yet.

--- 41 Countries/react/src/Header.js
+++ 41 Countries/react/src/Header.js
@@ -2,21 +2,35 @@
 
 function Header() {
     return (
-        <header className="bg-lightElements dark:bg-darkElements h-20 w-full shadow-lg px-20 flex justify-between items-center">
-            <h1 className="">Where in the world?</h1>
-            <button 
-                className="bg-transparent "
-                onClick={changeTheme}
-            >Dark mode</button>
+        <header className="bg-lightElements dark:bg-darkElements h-16 w-full shadow-lg px-20 flex justify-center items-center">
+            <div className="w-full max-w-7xl flex justify-between items-center">
+                <h1 className="text-xl">Where in the world?</h1>
+                <button 
+                    className="bg-transparent flex items-center transform translate-x-7 -translate-x-7"
+                    onClick={(event) => changeTheme(event)}
+                >
+                    <span className="
+                        inline-block rounded-full w-14 h-7 mr-3 bg-green-400 relative
+                        before:block before:transform before:transition-all before:absolute before:rounded-full before:w-6 before:h-6 before:top-0.5 before:right-0.5 before:bg-darkElements
+                    "></span>
+                    Dark mode
+                </button>
+            </div>
         </header>
     )
 }
 
-function changeTheme() {
+function changeTheme(event) {
+    // console.log(event.currentTarget.firstElementChild)
+    let span = event.currentTarget.firstElementChild;
     if (localStorage.theme === "dark") {
         localStorage.theme = "light";
+        span.classList.toggle("before:-translate-x-7", true)
+        span.classList.toggle("before:bg-white", true)
     } else {
         localStorage.theme = "dark";
+        span.classList.toggle("before:-translate-x-7", false)
+        span.classList.toggle("before:bg-white", false)
     }
     setTheme()
 }
