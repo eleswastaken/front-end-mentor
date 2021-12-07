eecf97994176be557a3f4c4096fe1d8877054436
eles linux <eleswastaken@gmail.com>
2021-12-07 14:58:13 +0600

    Set up Country cards container

--- 41 Countries/react/src/App.js
+++ 41 Countries/react/src/App.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 
 import Header from "./Header";
 import SearchForm from "./Search";
+import CardContainer from "./CardContainer"
 
 class App extends Component {
   constructor(props) {
@@ -10,7 +11,14 @@ class App extends Component {
         // this is supposed to be all data from the api
         // data: fetch("./data.json"),
         // this is countries showing on the screen
-        countries: [],
+        countries: [
+            {
+                name: "Italy",
+                capital: "Rome",
+                area: 301336.0,
+                population: 60665551,
+            },
+        ],
         filters: {
             region: "", 
             perpage: 8, 
@@ -31,6 +39,7 @@ class App extends Component {
         <Header />
         <main className="px-20 pt-14 ">
           <SearchForm handler={this.filterHandler} />
+          <CardContainer />
         </main>
       </div>
     );
---
+++ 41 Countries/react/src/CardContainer.js
@@ -0,0 +1,26 @@
+
+import {Component} from "react";
+
+class CardContainer extends Component {
+
+	render() {
+		let cards = [];
+		return(
+			<div className=" ">
+				<ul className=" ">
+					
+				</ul>
+			</div>
+		);
+	}
+}
+
+function Card() {
+	return(
+		<div>
+			
+		</div>
+	);
+}
+
+export default CardContainer;
