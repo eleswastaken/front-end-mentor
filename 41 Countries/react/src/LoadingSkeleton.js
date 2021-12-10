96cb238c3c23301d2b1b9fb4e55c8c3043deb43e
eles linux <eleswastaken@gmail.com>
2021-12-10 20:16:54 +0600

    Make info page loading skeleton responsive

--- 41 Countries/react/src/LoadingSkeleton.js
+++ 41 Countries/react/src/LoadingSkeleton.js
@@ -13,22 +13,22 @@ function LoadingSkeleton(props) {
 					</div>
 				</div>
 
-				<div className="info-content flex items-center">
-					<div className="skeleton flex-1 pr-16 min-h-full bg-lightElements dark:bg-darkElements rounded-md ">
+				<div className="info-content flex items-center flex-col lx:flex-row">
+					<div className="skeleton flex-1 lx:pr-16 mb-10 lx:mb-0 min-h-full bg-lightElements dark:bg-darkElements rounded-md ">
 						<img src="../images/aut.svg" className="w-[full] h-full opacity-0" width="500" height="400"/>
 					</div>
-					<div className="flex-1 pl-14">
-						<h1 className="skeleton text-2xl font-bold ml-[10px] mb-10 w-2/3 h-8 bg-lightElements dark:bg-darkElements rounded-md "></h1>
+					<div className="w-full lx:w-[auto] lx:flex-1 lx:pl-14">
+						<h1 className="skeleton text-2xl font-bold lx:ml-[10px] mb-10 w-2/3 h-8 bg-lightElements dark:bg-darkElements rounded-md "></h1>
 						<ul className="flex flex-wrap mb-10">
-							<li className="skeleton skeleton-p h-8 bg-lightElements dark:bg-darkElements rounded-md "></li>
-							<li className="skeleton skeleton-p h-8 bg-lightElements dark:bg-darkElements rounded-md "></li>
-							<li className="skeleton skeleton-p h-8 bg-lightElements dark:bg-darkElements rounded-md "></li>
-							<li className="skeleton skeleton-p h-8 bg-lightElements dark:bg-darkElements rounded-md "></li>
-							<li className="skeleton skeleton-p h-8 bg-lightElements dark:bg-darkElements rounded-md "></li>
-							<li className="skeleton skeleton-p h-8 bg-lightElements dark:bg-darkElements rounded-md "></li>
+							<li className="skeleton lx:ml-[10px] skeleton-p h-8 bg-lightElements dark:bg-darkElements rounded-md "></li>
+							<li className="skeleton lx:ml-[10px] skeleton-p h-8 bg-lightElements dark:bg-darkElements rounded-md "></li>
+							<li className="skeleton lx:ml-[10px] skeleton-p h-8 bg-lightElements dark:bg-darkElements rounded-md "></li>
+							<li className="skeleton lx:ml-[10px] skeleton-p h-8 bg-lightElements dark:bg-darkElements rounded-md "></li>
+							<li className="skeleton lx:ml-[10px] skeleton-p h-8 bg-lightElements dark:bg-darkElements rounded-md "></li>
+							<li className="skeleton lx:ml-[10px] skeleton-p h-8 bg-lightElements dark:bg-darkElements rounded-md "></li>
 						</ul>
-						<div className="flex">
-							<div className="skeleton ml-[10px] w-1/3 h-8 bg-lightElements dark:bg-darkElements rounded-md"></div>
+						<div className="flex flex-col lx:flex-row">
+							<div className="skeleton mb-3 lx:mb-0 lx:ml-[10px] w-1/3 h-8 bg-lightElements dark:bg-darkElements rounded-md"></div>
 							<ul className="flex flex-wrap max-w-[350px] gap-3 ml-3">
 								<li className="skeleton inline-block min-w-[100px] rounded-sm bg-lightElements dark:bg-darkElements text-center text-sm py-3 px-2"></li>
 								<li className="skeleton inline-block min-w-[100px] rounded-sm bg-lightElements dark:bg-darkElements text-center text-sm py-3 px-2"></li>
