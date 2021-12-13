4cff7a40d6c4cebd5933cb51b43ea535552e8232
eles linux <eleswastaken@gmail.com>
2021-12-13 20:39:46 +0600

    Prepare data for rendering

---
+++ src/js/main-data.js
@@ -0,0 +1,53 @@
+
+let cardData = [
+    {
+        imgURL: "./assets/screenshots/3.png",
+        imgAltText: "Blockchain-bitcoin currency price",
+        githubRepoURL: "https://github.com/eleswastaken/front-end/tree/master/43%20NFT%20Card",
+        livePreviewURL: "https://eleswastaken.github.io/front-end/43%20NFT%20Card/",
+        title: "NTF Card",
+        descriptionTitle: "",
+        descriptionText: ["The real challenge was animating eye on hover.", "I didn't want to clutter markup with 'meaningless' elements, plus I probably would've had to use js. So I went for pseudo elements instead.", "Pseudo elements of a link are still a part of the link, meaning they would still work even though on top of the link."],
+        tags: ["animation", "html", "css"],
+        dateStr: "Decemeber 3rd, 2021",
+        date: new Date("2021-12-03"),
+    },
+    {
+        imgURL: "./assets/screenshots/1.png",
+        imgAltText: "Room - Furniture Company Landing Page",
+        githubRepoURL: "https://github.com/eleswastaken/front-end/tree/master/31%20Room%20LP",
+        livePreviewURL: "https://eleswastaken.github.io/front-end/31%20Room%20LP/",
+        title: "Room Landing Page",
+        descriptionTitle: "",
+        descriptionText: ["This challenge I used the power of the flexbox to line out everthing.", "This was my first challenge using Tailwind CSS."],
+        tags: ["mobile-first", "tailwind css", "vanilla js", "html", "css"],
+        dateStr: "November 16th, 2021",
+        date: new Date("2021-11-16"),
+    },
+    {
+        imgURL: "./assets/screenshots/2.png",
+        imgAltText: "Sneakers landing page",
+        githubRepoURL: "https://github.com/eleswastaken/front-end/tree/master/42%20E-commerce%20product%20page",
+        livePreviewURL: "https://eleswastaken.github.io/front-end/42%20E-commerce%20product%20page/",
+        title: "Sneakers Product Page",
+        descriptionTitle: "",
+        descriptionText: ["Almost all of my focus was directed to accessibility and markup.", "The cart is an array of objects that each represents an item in the cart. When array changes the cart is rerendered.", "Made use of local storage to save and render cart items."],
+        tags: ["vanilla js", "scss", "html", "css"],
+        dateStr: "November 14th, 2021",
+        date: new Date("2021-11-14"),
+    },
+    {
+        imgURL: "",
+        imgAltText: "",
+        githubRepoURL: "",
+        livePreviewURL: "",
+        title: "",
+        descriptionTitle: "",
+        descriptionText: [],
+        tags: [],
+        dateStr: new Date(),
+        date: new Date(),
+    }
+];
+
+// Sat Dec 11 08:58:59 2021 +0600
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
-import cards from "../../main-data.js";
 
 
 // implement insert cards into a container from cards
 
-console.log(cards)
+console.log(cardData)
